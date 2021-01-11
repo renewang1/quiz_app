0d48d44e37284113a6561aa124796987f088644d
Rene Wang <rene.wang1@hotmail.com>
2021-01-11 16:32:50 +0000

    completed route handling for inserting into questions table

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -12,25 +12,77 @@ module.exports = (db) => {
   //Creating new quiz
   router.post("/", (req, res) => {
     const queryParams = [];
-    queryParams.push(rq.session.user_id, req.body.title, req.body.description, req.body.URL, req.body.private)
+    const id = req.body.id
+    const title = req.body.title
+    const description = req.body.description
+    const URL = req.body.URL
+    const is_private = req.body.is_private
+    // queryParams.push(req.session.user_id, req.body.title, req.body.description, req.body.URL, req.body.private)
+    queryParams.push(id, title, description, URL, is_private)
     let returnData = {};
-    db.query(`
-      INSERT INTO quizzes (creator_id, title, description, URL, private)
-      VALUES (1$, 2$, 3$, 4$, 5%)
+    return db.query(`
+      INSERT INTO quizzes (creator_id, title, description, URL, is_private)
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
     `, queryParams)
       .then(res => {
-        returnData.table1 = res;
-        queryParams.push(returnData.table1.res.rows.id, req.body.question)
-        db.query(`
-          INSERT INTO questions (quiz_id, question)
-          VALUES (1$, 2$)
-          RETURNING *;
-        `, [queryParams[queryParams.length - 1], queryParams[queryParams.length]])
+        returnData.table1 = res.rows;
+        const questions = Object.keys(req.body).filter(item => item.slice(0, 8) === "question");
+        let quiz_id = returnData.table1[0].id;
+        let questionValues = [];
+        for (const question of questions) {
+          questionValues.push(req.body[question]);
+        }
+        // queryParams.push(returnData.table1.res.rows.id, req.body.question)
+        let queryString = `
+        INSERT INTO questions (quiz_id, question)
+        `;
+        for (let i = 0; i < questions.length; i++) {
+          if (i === 0) {
+            queryString += `
+            VALUES ($1, $${i + 2}),
+            `
+          } else if (i !== questions.length - 1) {
+            queryString += `
+            ($1, $${i + 2}),
+            `
+          } else {
+            queryString += `
+            ($1, $${i + 2})
+            `
+          }
+        };
+        queryString += `
+        RETURNING *;
+        `;
+        questionValues.unshift(quiz_id);
+        return db.query(queryString, questionValues)
       })
       .then(res => {
         returnData.table2 = res;
-        queryParams.push(returnDara.table2.res.rows.id, req.body.answer, req.body.correct)
+        // queryParams.push(returnData.table2.res.rows.id, req.body.answer, req.body.correct)
+
+        let answers = Object.keys(req.body).filter(item => item.slice(0, 6) === 'answer')
+        let answerValues = {};
+
+        for (const answer of answers) {
+          if (answerValues[answer.slice(6, 7)]) {
+          answerValues[answer.slice(6, 7)].push(req.body[answer]);
+          }	else {
+          answerValues[answer.slice(6, 7)] = [req.body[answer]];
+          }
+        }
+
+        console.log(answers)
+        console.log(answerValues)
+
+
+
+
+
+        let queryString = `
+          INSERT INTO answers (question_id, answer, correct)
+        `
         db.query(`
         INSERT INTO answers (question_id, answer, correct)
         VALUES ($1, $2, $3)
