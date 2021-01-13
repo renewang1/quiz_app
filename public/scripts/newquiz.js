d792d0ddb9219119f81694d7e42b55a7cb86e5d0
Rene Wang <rene.wang1@hotmail.com>
2021-01-13 01:35:41 +0000

    implemented newquiz jquery scripts to dynamically add input areas for questions and answers

---
+++ public/scripts/newquiz.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+  $(document).on('click', '.addquestionbutton', function() {
+    console.log(this)
+    appendQuestion(this);
+    restoreButtons();
+    questionNumber++;
+  })
+
+  $(document).on('click', '.addanswerbutton', function() {
+    appendAnswer(this);
+  })
+
+});
+
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+let questionNumber = 2;
+
+const createQuestionElement = function() {
+  const $question = $(`
+  <div class="question${questionNumber}">
+    <div>
+      <label for="question${questionNumber}">Question:</label>
+      <input type="text" name="question${questionNumber}" class="question${questionNumber}">
+    </div>
+    <span>
+      <input type="text" name="answers" class="answers">
+      <input type="radio" name="answer${questionNumber}">
+    </span>
+    <span>
+      <input type="text" name="answers" class="answers">
+      <input type="radio" name="answer${questionNumber}">
+    </span>
+  </div>
+  <button type="button" class="addanswerbutton" name="button${questionNumber}">Add another answer</button>
+  <div>
+    &nbsp;
+  </div>
+  `)
+  return $question;
+}
+
+const createAnswerElement = function(num) {
+  const $answer = $(`
+    <span>
+      <input type="text" name="answers" class="answers">
+      <input type="radio" name="answer${num}">
+    </span>
+  `);
+  return $answer;
+}
+
+const createBottomButtons = function() {
+  const $bottomButtons = $(`
+  <button type="button" class="addquestionbutton">Add another question</button>
+  <div class="bottombuttons">
+    <div class="makeprivatesections">
+      <label for="makeprivatebox" class="makequizprivate">Make private</label>
+      <input type="checkbox" name="makeprivatebox">
+    </div>
+    <button type="submit" class="createquizbutton">Finish quiz</button>
+  </div>
+  `);
+  return $bottomButtons;
+}
+
+// const createAnswerButton = function() {
+//   const $answerbutton = $(`
+//     <button type="button" class="addanswerbutton">Add another answer</button>
+//   `)
+//   return $answerbutton;
+// }
+
+const appendQuestion = function() {
+  let $question = createQuestionElement();
+  $('.maininputform').append($question);
+}
+
+const appendAnswer = function(data) {
+  const questionNum = data.parentElement[2].name.slice(-1);
+  let $answer = createAnswerElement(questionNum);
+  $(`.question${data.name.slice(-1)}`).append($answer);
+}
+
+const restoreButtons = function() {
+  $(".bottombuttons").empty();
+  $(".addquestionbutton").remove();
+  let $bottomButtons = createBottomButtons();
+  $(".maininputform").append($bottomButtons);
+}
+
+// const appendAnswerButton = function() {
+//   let $answerbutton = createAnswerButton();
+//   $('.maininputform').append($answerbutton);
+// }
+
+// const renderQuizzes = function(data) {
+//   $('.quiz-container').empty();
+//   for (let quiz of data) {
+//     let $quiz = createQuizElement(quiz);
+//     $('.quiz-container').append($quiz);
+//   }
+// }
+
